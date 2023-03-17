dd92cc97cafe1b6c42ba40d55f7ee4115977a4e0
leehyeyeong <elto789@naver.com>
2023-03-17 19:07:54 +0900

    redirect, rewrite

--- next.config.js
+++ next.config.js
@@ -1,6 +1,26 @@
 /** @type {import('next').NextConfig} */
+
+const API_KEY = "d44328af66c7b912250dca6b1d3216a0";
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/old-blog/:path*",
+        destination: "/new-blog/:path*",
+        permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/movies",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+      }
+    ]
+  }
+};
 
 module.exports = nextConfig
